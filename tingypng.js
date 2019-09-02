9eea5a84d398b61765f40fe70c7a3c4a4d1318b4
taosue <taosue314@gmail.com>
2019-09-02 16:07:55 +0800

    update nav

--- tingypng.js
+++ tingypng.js
@@ -7,11 +7,3 @@ tinypng
   .then(e => {
     console.log("压缩完了", e);
   });
-// tinypng
-//   .minifiyFile(
-//     "../src/190306PizzaIWD/assets/tab/24171551954476_.pic_hd.jpg",
-//     "../src/190306PizzaIWD/assets/tab/contain_realGold.png"
-//   )
-//   .then(e => {
-//     console.log("压缩完了", e);
-//   });
